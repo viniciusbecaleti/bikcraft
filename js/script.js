393679f88c520070a4386b52bcbe7594818bbf0a
Vinicius <vinicius@becaleti.com>
2022-03-04 12:18:58 -0300

    js link menu ativo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("Teste")
+const url = location.href
+const links = document.querySelectorAll(".header-menu a")
+
+links.forEach(link => {
+   if (url.includes(link.href)) {
+      link.classList.add("ativo")
+   }
+});
