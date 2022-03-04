2f6b16c0910a6a568542994857e1419b3b7000db
Vinicius <vinicius@becaleti.com>
2022-03-04 13:23:46 -0300

    js ativar itens do orçamento de acordo com os parametros passados na URL

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar links menu
 const url = location.href
 const links = document.querySelectorAll(".header-menu a")
 
@@ -6,3 +7,18 @@ links.forEach(link => {
       link.classList.add("ativo")
    }
 });
+
+// Ativar itens do orçamento
+const parametros = new URLSearchParams(location.search)
+
+function ativarProduto(parametro) {
+   console.log(parametro);
+   
+   const elemento = document.getElementById(parametro)
+   
+   if (elemento) {
+      elemento.checked = true
+   }
+}
+
+parametros.forEach(ativarProduto);
